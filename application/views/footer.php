77c80beb51467e6d8df1ed119df5716420c106ec
nikhilaasnikar <nikhilasnikar@yahoo.com>
2019-12-25 20:51:12 +0000

    footer home page

---
+++ application/views/footer.php
@@ -0,0 +1,106 @@
+ <section class="cid-qTkAaeaxX5" id="footer1-2">
+    <div class="container">
+        <div class="media-container-row content text-white">
+            <div class="col-12 col-md-3">
+                <div class="media-wrap">
+                    <a href="<?=BASE?>">
+                        <img src="<?php echo base_url();?>assets/images/logo2.jpg" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style">
+                <h5 class="pb-3">
+                    Address
+                </h5>
+                <p class="mbr-text">
+                    1234 Street Name
+                    <br>City, AA 99999
+                </p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style">
+                <h5 class="pb-3">
+                    Contacts
+                </h5>
+                <p class="mbr-text">
+                    Email: support@bandb.com
+                    <br>Phone: +1 (0) 000 0000 001
+                    <br>Fax: +1 (0) 000 0000 002
+                </p>
+            </div>
+            
+        </div>
+        <div class="footer-lower">
+            <div class="media-container-row">
+                <div class="col-sm-12">
+                    <hr>
+                </div>
+            </div>
+            <div class="media-container-row mbr-white">
+                <div class="col-sm-6 copyright">
+                    <p class="mbr-text mbr-fonts-style">
+                        © Copyright 2019 Bed&amp;Breakfast - All Rights Reserved
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <div class="social-list align-right">
+                        <div class="soc-item">
+                            <a href="<?=BASE?>" target="_blank">
+                                <span class="socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="<?=BASE?>" target="_blank">
+                                <span class="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="<?=BASE?>" target="_blank">
+                                <span class="socicon-youtube socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="<?=BASE?>" target="_blank">
+                                <span class="socicon-instagram socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="<?=BASE?>" target="_blank">
+                                <span class="socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="<?=BASE?>" target="_blank">
+                                <span class="socicon-behance socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
+  <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src='<?php echo base_url();?>js/app.js'></script>
+  <script src="<?php echo base_url();?>assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url();?>assets/popper/popper.min.js"></script>
+  <script src="<?php echo base_url();?>assets/tether/tether.min.js"></script>
+  <script src="<?php echo base_url();?>assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="<?php echo base_url();?>assets/dropdown/js/nav-dropdown.js"></script>
+  <script src="<?php echo base_url();?>assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="<?php echo base_url();?>assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="<?php echo base_url();?>assets/masonry/masonry.pkgd.min.js"></script>
+  <script src="<?php echo base_url();?>assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="<?php echo base_url();?>assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+  <script src="<?php echo base_url();?>assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+  <script src="<?php echo base_url();?>assets/parallax/jarallax.min.js"></script>
+  <script src="<?php echo base_url();?>assets/theme/js/script.js"></script>
+  <script src="<?php echo base_url();?>assets/gallery/player.min.js"></script>
+  <script src="<?php echo base_url();?>assets/gallery/script.js"></script>
+  <script src="<?php echo base_url();?>assets/slidervideo/script.js"></script>
+  <script src="<?php echo base_url('assets/jquery.validate.min.js');?>"></script>
+  <script src="<?php echo base_url('assets/jquery.validate-init.js');?>"></script>
+  <script type="text/javascript" src='<?php echo base_url();?>js/custom.js'></script>
+</body>
+</html>
